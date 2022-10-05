94c07082d1fe22cdfb58d615ffe54729fb3129bd
Hafil Latheef <iolathief108@gmail.com>
2022-10-05 10:19:23 +0530

    slider updates

--- comps/slider/hero-slider.tsx
+++ comps/slider/hero-slider.tsx
@@ -2,8 +2,6 @@ import {useSnapshot} from 'valtio';
 import frontState from '../../states/front';
 import Slider, {Settings} from '@ant-design/react-slick';
 import {isDevelopment} from '../../lib/config';
-import {useEffect, useState} from 'react';
-import Image from 'next/image';
 import {useHasHydrated} from '../../lib/utils';
 
 
@@ -13,7 +11,6 @@ type ItemProps = {
 const Item = ({url}: ItemProps) => {
     return (
         <div style={{
-            // width: '100%',
             height: '100%',
         }}>
             <img
@@ -32,9 +29,6 @@ const Item = ({url}: ItemProps) => {
                     }, 250);
                 }}
             />
-            {/*<Image src={url} height={getSliderImageHeight()} width={getSliderImageWidth()} objectFit={'cover'} onLoad={() => {*/}
-            {/*    frontState.mainBannerLoaded = true;*/}
-            {/*}}/>*/}
         </div>
     );
 };
@@ -57,9 +51,7 @@ export function Carousel(props: CarouselProps) {
         slidesToScroll: 1,
         autoplay: !isDevelopment,
         autoplaySpeed: 3000,
-        // lazyLoad: 'progressive',
         lazyLoad: !hasHydrated ? undefined : ((mainBannerLoaded && noDodLoaded > 3) ? undefined : 'ondemand'),
-
         responsive: [
             {
                 breakpoint: 768,
@@ -93,13 +85,6 @@ export function Carousel(props: CarouselProps) {
 
 export function HomeSlider() {
     const {sliderImageUrls} = useSnapshot(frontState);
-    const [ssr, setSsr] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setSsr(false);
-        }, 5000);
-    }, []);
 
     if (sliderImageUrls.length === 0) {
         return null;
@@ -111,32 +96,4 @@ export function HomeSlider() {
             <Carousel urls={sliderImageUrls}/>
         </div>
     );
-}
-
-function getSliderImageHeight() {
-    if (typeof window === 'undefined') {
-        return 0;
-    }
-    const width = window.innerWidth;
-    if (width < 768) {
-        return 200;
-    }
-    if (width < 992) {
-        return 300;
-    }
-    return 400;
-}
-
-function getSliderImageWidth() {
-    if (typeof window === 'undefined') {
-        return 0;
-    }
-    const width = window.innerWidth;
-    if (width < 768) {
-        return 200;
-    }
-    if (width < 992) {
-        return 300;
-    }
-    return 1200;
 }
